7951431c0cea4fa851fdb571564b07fae034b9d4
amirmohammadfathi <amitfathi1380@gmail.com>
2023-06-12 10:30:05 +0330

    change path of captcha.png

--- core/settings.py
+++ core/settings.py
@@ -117,7 +117,7 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-path_of_captcha_picture = "/home/sedreh01/Sedreh/amf/captcha_app/captcha_pictures"
+path_of_captcha_picture = "captcha_app/captcha_pictures"
 
 
 
